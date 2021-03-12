e74a0a3eabdd80c7253aec0f33669600b0155e85
kimothokamau <kimothokamau@gmail.com>
2021-03-12 01:28:03 -0600

    Using concerns to make some articles archived or public

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -6,8 +6,8 @@ class CommentsController < ApplicationController
   end
 
   private
-    def comment_params
-      params.require(:comment).permit(:commenter, :body)
-    end
-end
 
+  def comment_params
+    params.require(:comment).permit(:commenter, :body, :status)
+  end
+end
