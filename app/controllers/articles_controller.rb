23bedb425bbfd0787040c2d5837799f017412cd8
kimothokamau <kimothokamau@gmail.com>
2021-03-12 00:49:17 -0600

    Add edit , combine edit and new to use partials

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -21,9 +21,23 @@ class ArticlesController < ApplicationController
     end
   end
 
-  private
-    def article_params
-      params.require(:article).permit(:title, :body)
-    end
-end
+  def edit
+    @article = Article.find(params[:id])
+  end
 
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :body)
+  end
+end
